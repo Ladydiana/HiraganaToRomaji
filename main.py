e8b526f53a8de66e6cdc3a8e9b2874804bb0fdb5
Ladydiana <diana.culincu@gmail.com>
2019-04-23 01:57:07 +0300

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 
+
+@author: Culincu Diana Cristina
+"""
+
+from IOfunctions import convertToRomaji 
+from IOfunctions import readHiraganaFromKeyboard
+
+
+conv = convertToRomaji(readHiraganaFromKeyboard())
+print(conv)
+
+#ぎろの
